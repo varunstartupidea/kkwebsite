fc8bfeb8002db25fed591c27469d826318c6a29a
Varun Bollam <varunstartupidea@gmail.com>
2026-03-16 00:12:30 +0530

    fix: correct next.config.js syntax error

--- next.config.js
+++ next.config.js
@@ -5,11 +5,5 @@ const nextConfig = {
   assetPrefix: '/kkwebsite/',
   images: { unoptimized: true },
 };
-module.exports = nextConfig;
-```
 
-Then open **`.gitignore`** and make sure these lines are in it:
-```
-.next
-out
-node_modules
+module.exports = nextConfig;
